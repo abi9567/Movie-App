88b9ac3fb7495af35f3a6eb457face24bd8b6d5e
abi <abicherayakkattu2012@gmail.com>
2024-07-29 23:27:03 +0530

    change version code to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.abi.movieapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
