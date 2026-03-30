0d492fb7f5ae8be6bf4765590f924288a74f0c34
Abi <abicherayakkattu2012@gmail.com>
2026-03-30 20:31:23 +0530

    update build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.abi.movieapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
